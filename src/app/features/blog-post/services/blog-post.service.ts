ec11a335ed1e56adfaefc9495102e196f7d20178
Lenovo <hassanyousef.de@gmail.com>
2023-11-05 16:18:40 +0100

    UI - submit form to Update BlogPost and categories

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -4,6 +4,7 @@ import { AddBlogPost } from '../models/add-blog-post.model';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { BlogPost } from '../models/blog-post.model';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,11 @@ export class BlogPostService {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
   }
 
+  getBlogPostById(id:string) : Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
+  }
+
+  updateBlogPost(id: string, updatedBlogPost: UpdateBlogPost): Observable<BlogPost> {
+    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`, updatedBlogPost);
+  }
 }
